3483bbb70c4a5ae50110cb061e1e72e756e8d01d
CatalysePro <catalyse.pro@proton.me>
2024-01-10 16:47:05 +0100

    JS script update & comments + ReadMe file

---
+++ ReadMe.txt
@@ -0,0 +1,14 @@
+Internal ref P_I_D_1_3
+
+Done on Jan 5th 2024
+
+Github pages : https://catalysepro.github.io/OC_P5/
+
+repo Github : https://github.com/CatalysePro/OC_P5.git
+
+
+Carousel in a banner :
+
+From existing and "limited" files, the exercise consists in (slighly) modifying the html file for arrows insertion and
+adding a JS code to make the carousel existing and functionning (bullets points and arrows click + bullet point 
+indicators synchronization).
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,8 @@
 
 
 
-// 0.0 (initial & unique inputs) Define an array of slides, each with an image and a tagline
+// 0.0 (initial & unique inputs) Define an array of slides, each with an image and a tagline 
+//- non optimal (separate technical logic & presentation logic)
 
 
 const slides = [
@@ -46,11 +47,13 @@ let currentIndex = 0; // current index initiliazation
 // 3.  Slide Display functions 
 
 function nextSlide () {
+	console.log("Function nextSlide called !");				// Console.log to verify if the function is called when needed
 	currentIndex = (currentIndex + 1) % slidesNb  			// modulo operator % gives an integer number (rest of an euclidian division)
 	displaySlide () // call function : specific slide display
 }
 
 function prevSlide () {
+	console.log("Function prevSlide called !");				// Console.log to verify if the function is called when needed
 	currentIndex = (currentIndex - 1 + slidesNb) % slidesNb
 	displaySlide () // call function : specific slide display
 }
@@ -68,24 +71,56 @@ for (let i = 0; i < slidesNb; i++) {			// Loop through each slide to create corr
 	bannerDots.appendChild(dot);				// Append the created <span> element (dot) to the bannerDots container
 	
 	// Add event listener to each dot
-    dot.addEventListener("click", () => {
-        currentIndex = i;
-        displaySlide();
-    });
+
+			// basic approach not activated
+
+    // dot.addEventListener("click", () => {
+    //     currentIndex = i;
+    //     displaySlide();
+    // });
+
+		// activated approach : with an event listener synchronized with the pics and dots numbers
+	
+		dot.addEventListener("click", () => {
+			const dotNumber = i + 1; 							// Adds 1 to the index to obtain the "human" numbering
+			console.log(`Click on the dot # ${dotNumber}. Image order nb : ${dotNumber}`); // Uses variable dotNumber as pics nb
+			currentIndex = dotNumber - 1; 		// insures the consistency with the "machine" numbering used by currentIndex itself used by the rest of the code
+			displaySlide(); // function call
+		});
 }
 
 // 6. Events listeners for arrows clicks
 
-leftArrow.addEventListener("click", prevSlide);
-rightArrow.addEventListener("click", nextSlide);
+
+// rightArrow.addEventListener("click", nextSlide); // basic instruction
+
+rightArrow.addEventListener("click", function(event) {				// event listener with console.log
+    console.log("Click on the right arrow", event);
+    nextSlide();
+});
+
+
+// leftArrow.addEventListener("click", prevSlide); // basic instruction
+
+leftArrow.addEventListener("click", function(event) {				// event listener with console.log
+    console.log("Click on the left arrow", event);
+    prevSlide();
+});
+
 
 
 // 4. Specific slide display function
 
 function displaySlide() {
-    const slide = slides[currentIndex];
-    bannerPic.src = `./assets/images/slideshow/${slide.image}`;
-    bannerTagLine.innerHTML = slide.tagLine;
+    const slide = slides[currentIndex];					// Retrieving the current slide from the slides array at corresponding index.
+    bannerPic.src = `./assets/images/slideshow/${slide.image}`;		// Gives the path (.src) of the previously declarated constant 
+																	// " bannnerPic", ${slide.image} is used to embed the value 
+																	// of the image property of the  object (slide) into a 
+																	// string - easier way to concatenate strings and variables.
+    
+	bannerTagLine.innerHTML = slide.tagLine;			// The instruction "inner" modify the previously declarated constant 
+														// " bannnerTagLine" (which corresponds to the element p) and
+														// assigns to it the value defined for "tagLine" in the initial declaration
 
     // Remove dot_selected class from all dots except on the selected slide
     const dots = bannerDots.querySelectorAll(".dot");
